9fadfa67f3a16bb58660ea3cab2f015f2af839c1
hellosiyam <hellosiyambd@gmail.com>
2025-02-03 16:56:28 +0600

    mi-7-mo-38-react

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,19 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name= 'Laptop' price = '25000'></Device>
+      <Device name= 'Mobile'></Device>
+      <Device name= 'Computer'></Device>
+      <Device name= 'Car'></Device>
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Student grad = '9th' point = '95'></Student>
+      <Developer></Developer>
     </>
   )
+}
+
+function Device(props) {
+  return <h2>This Device : {props.name} price: {props.price}</h2>
 }
 
 function Person() {
@@ -21,5 +29,31 @@ function Person() {
   const money = 15;
   const person = {name : 'Siyam', age : 22}
   return <h1>I am a {person.name}. I am {age + money} old</h1>
+}
+
+function Student(props) {
+  return (
+    <div className='students'>
+      <h3>This is a Student</h3>
+      <p>Name:</p>
+      <p>Grad: {props.grad}</p>
+      <p>Point: {props.point}</p>
+    </div>
+  )
+}
+
+function Developer() {
+  const devStyle = {
+    margin : '20px',
+    padding : '10px',
+    border : '2px solid green',
+    borderRadius : '16px',
+  }
+  return(
+    <div style={devStyle}>
+      <h1>Dev:</h1>
+      <p>Coding:</p>
+    </div>
+  )
 }
 export default App
